1c2c8430db75565c26f5db4adf700e9648d52709
Ardhan12 <arief.ramadhan43@gmail.com>
2022-10-03 11:41:14 +0700

    Initial Commit

---
+++ MotorcycleApp/MotorcycleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorcycleAppApp.swift
+//  MotorcycleApp
+//
+//  Created by Arief Ramadhan on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MotorcycleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
